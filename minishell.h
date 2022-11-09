c93fbd9d6115a9ebead1902d70c0215b1c28ed72
Raphael Perrin <raphouperrin@gmail.com>
2022-11-09 14:41:41 +0100

    coucou

--- minishell.h
+++ minishell.h
@@ -1,27 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: raphaelperrin <raphaelperrin@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/09 11:22:44 by raphaelperr       #+#    #+#             */
-/*   Updated: 2022/11/09 14:40:47 by raphaelperr      ###   ########.fr       */
+/*   Created: 2022/11/09 12:05:32 by raphaelperr       #+#    #+#             */
+/*   Updated: 2022/11/09 12:05:52 by raphaelperr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "libft/libft.h"
 
-int	main(int argc, char const **argv)
-{
-	char	*command_buffer;
-
-	printf("coucou maxime");
-	while (1)
-	{
-		command_buffer = readline("test> ");
-		if (ft_strlen(command_buffer) > 0)
-			printf("%s\n", command_buffer);
-	}
-	return (0);
-}
