3e4feacc03569c36ef344a666ab893940d8aad91
Maxime Uteza <muteza@c4r1p4.42nice.fr>
2023-02-01 16:50:22 +0100

    glouglou

--- pipe/new_pipe.c
+++ pipe/new_pipe.c
@@ -6,11 +6,46 @@
 /*   By: muteza <muteza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 14:18:39 by muteza            #+#    #+#             */
-/*   Updated: 2023/02/01 11:46:42 by muteza           ###   ########.fr       */
+/*   Updated: 2023/02/01 16:08:29 by muteza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+void	get_all(int fd, int fdout)
+{
+	char	*buff;
+
+	while (read(fd, &buff, 1) > 0)
+		write(fdout, buff, 1);
+}
+
+void	redir(t_data *data, t_lst *lst)
+{
+	if (check_is_builtin(data->str[0]) == 1)
+		data->path = get_path(data->envp, data->str[0]);
+	data->k = 1;
+	dup2(data->save, 0);
+	if (lst->fdout)
+		get_all(data->fd[1], lst->fdout);
+	if (data->save != 0)
+		close(data->save);
+	close(data->fd[0]);
+	close(data->fd[1]);
+	if (builtin_pipe(data, lst) == 0)
+		exit(0);
+	else
+		execve(data->path, data->str, data->envp);
+}
+
+int	check_redir(t_data *data)
+{
+	printf("%d\n", data->out);
+	printf("%d\n", data->in);
+	if (data->out == 0 && data->in == 0)
+		return (1);
+	return (0);
+}
 
 void	more_pipe(t_data *data, t_lst *lst, int i)
 {
@@ -18,7 +53,7 @@ void	more_pipe(t_data *data, t_lst *lst, int i)
 	if (pipe(data->fd) == -1)
 		perror("pipe blem\n");
 	data->id = fork();
-	if (data->id == 0)
+	if (data->id == 0 && check_redir(data) == 1)
 	{
 		if (check_is_builtin(data->str[0]) == 1)
 			data->path = get_path(data->envp, data->str[0]);
@@ -31,11 +66,12 @@ void	more_pipe(t_data *data, t_lst *lst, int i)
 		close(data->fd[0]);
 		close(data->fd[1]);
 		if (builtin_pipe(data, lst) == 0)
-			exit(0);//kill(data->id, SIGKILL);
+			exit(0);
 		else
 			execve(data->path, data->str, data->envp);
 	}
-	// printf("%d\n\n", data->k );
+	else if (data->id == 0)
+		redir(data, lst);
 	if (data->save != 0)
 		close(data->save);
 	data->save = data->fd[0];
